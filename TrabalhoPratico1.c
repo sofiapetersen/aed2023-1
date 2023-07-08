05c5d0e7e42227ad50f7fd3ef31e659e7c47a464
Sofia <sppessil@inf.ufpel.edu.br>
2023-07-08 18:44:11 -0300

    Código finalizado

--- TrabalhoPratico1.c
+++ TrabalhoPratico1.c
@@ -7,50 +7,51 @@ char *shortestCompletingWord( char *licensePlate, char **words, int wordsSize );
 
 int main(){
 
-    char licensePlate[] = "1s3 PSt", *words[4] = {"step", "steps", "stripe", "stepple"}, *finalWord;
-
+    char licensePlate[] = "1s3 PSt";
+    char *words[4] = {"step", "steps", "stripe", "stepple"};
     int size = 4;
 
-    *finalWord = shortestCompletingWord( licensePlate, words, size );
+    char *finalWord = shortestCompletingWord( licensePlate, words, size );
     printf( "The shortest is: %s", finalWord );
 
     return 0;
 }
 
 char *shortestCompletingWord( char *licensePlate, char **words, int wordsSize ){
-    int countLicensePlate[15] = {0}, countWord[20] = {0};        //pra contar os aparecer de cada caracter em licenseplate e em cada word
-    int j, i, k, valid = 1;
+    int countLicensePlate[26] = {0};  // Array para contar as ocorrências de cada caractere em licensePlate
     char *shortWord = NULL;
 
     //pesquisei pra ver no license se eh numero ou caracter, e como transformar tudo em minuscula
 
 
-    for(i=0; i < (int)strlen(licensePlate); i++){
-        if ( isalpha( licensePlate[i] ) ){      //se for letra
-            countLicensePlate[ tolower( licensePlate[i] ) - 'a' ]++;    //transformar em minuscula
+    for (int i = 0; i < (int)strlen(licensePlate); i++) {
+        if (isalpha(licensePlate[i])) {      //se for letra
+            countLicensePlate[tolower(licensePlate[i]) - 'a']++;    //transformar em minuscula
 
         }
-
-
-          for(j=0; j < wordsSize; j++){
-            for( i=0; i < ( int )strlen(words[j]); i++ ){
-                countWord[tolower( words[j][i] ) - 'a'];    //transformando todas as letras das words em minusculas
-
+        
+          for (int i = 0; i < wordsSize; i++) {
+            int countWord[26] = {0};        //pra contar os aparecer de cada caracter em words
+            for (int j = 0; j < (int)strlen(words[i]); j++) {
+                 if (isalpha(words[i][j])) {
+                countWord[tolower(words[i][j]) - 'a']++;    //transformando todas as letras das words em minusculas
+                }
             }
-            for( k=0; k < 15; k++ ){        // percorre as duas arrays
-                if( countLicensePlate[k] > countWord[j] ){
+            int valid = 1;
+            for (int k = 0; k < 26; k++) {    // percorre as duas arrays
+                if (countLicensePlate[k] > countWord[k]) {
                     valid = 0;
                     break;
                 }
             }
                 // ai fez aqui ver se a palavra tem todos os caracteres que license tá pedindo
 
-        if( valid && ( shortWord == NULL || strlen( words[j] < shortWord[j] ) ) ){
-            shortWord = words[j];
+        if (valid && (shortWord == NULL || strlen(words[i]) < strlen(shortWord))) {
+            shortWord = words[i];
         }
             // entao se a word que ta vendo tiver os caracteres e for a mais curta, shortWord vai receber essa word e vai ser retornada
     
         }
 }
-return shortWord;
+    return shortWord;
 }
