79c3c9c364c7617f74c156a220c02c4a791912b9
Sofia <sppessil@inf.ufpel.edu.br>
2023-07-07 22:29:00 -0300

    adicionei umas funcoes do ctype

--- TrabalhoPratico1.c
+++ TrabalhoPratico1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char *shortestCompletingWord( char *licensePlate, char **words, int wordsSize );
 
@@ -17,11 +18,17 @@ int main(){
 }
 
 char *shortestCompletingWord( char *licensePlate, char **words, int wordsSize ){
-
+    int countLicensePlate[15] = {0};        //pra contar os aparecer de cada caracter em licenseplate
     int j, i;
-    char final[15];
+
+    //pesquisei pra ver no license se eh numero ou caracter, e como transformar tudo em minuscula
+
 
     for(i=0; i < (int)strlen(licensePlate); i++){
+        if ( isalpha( licensePlate[i] ) ){      //se for letra
+            countLicensePlate[ tolower( licensePlate[i] ) - 'a' ]++;    //transformar em minuscula
+
+        }
         for(j=0; j < wordsSize; j++){
             if (words[j] == licensePlate[i]){
                 final[i] = words[j];
